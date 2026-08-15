c64a7b4f05608988072560fcbdd05dbe9d14499b
dustns09060-eng <dustns09060@gmail.com>
2026-08-15 18:35:38 +0900

    config.js 업데이트

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-window.YEOWOOBANG_CONFIG = {
-  API_URL: ""
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbwO1A8OHL_Tw48uisPPVOLk4PbnOVyiQlnoJ6EivB6_FF0zTZKXJFY6OMrv78VNcaGMdQ/exec"
 };
