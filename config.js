d2dd67be3e73d3072bcee9c11551b52943f13971
dustns09060-eng <dustns09060@gmail.com>
2026-08-17 16:20:14 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.YEOWOOBANG_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycby2jcmEvTTbVJ9PpZ8jYI2703AQOeCj0KZ8d7Clun4as_lyPexxcmdfjgOZKdzFveaoyw/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbwO1A8OHL_Tw48uisPPVOLk4PbnOVyiQlnoJ6EivB6_FF0zTZKXJFY6OMrv78VNcaGMdQ/exec"
 ,
   SERVICE_NAME: "YEOWOOBANG CHECK",
-  BUILD: "v31-api-comments"
+  BUILD: "v32-facebook-comments-live"
 };
